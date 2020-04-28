0d5a9176cc530503b7238f24857269f2bf481333
andres <andferrod3@alum.us.es>
2020-04-28 12:29:30 +0200

    Task 044: finished

--- src/test/java/org/springframework/samples/petclinic/web/PickUpRequestControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/PickUpRequestControllerTests.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import javax.activity.InvalidActivityException;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
@@ -32,32 +34,34 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@WebMvcTest(controllers = PickUpRequestController.class, includeFilters = @ComponentScan.Filter(value = PetTypeFormatter.class, type = FilterType.ASSIGNABLE_TYPE), excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class), excludeAutoConfiguration = SecurityConfiguration.class)
+@WebMvcTest(controllers = PickUpRequestController.class, includeFilters = @ComponentScan.Filter(value = PetTypeFormatter.class, type = FilterType.ASSIGNABLE_TYPE),
+	excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class), excludeAutoConfiguration = SecurityConfiguration.class)
 public class PickUpRequestControllerTests {
 
-	private static final int TEST_PICK_UP_REQUEST_ID = 1;
+	private static final int		TEST_PICK_UP_REQUEST_ID	= 1;
 
-	private static final int TEST_OWNER_ID = 1;
-	
-	private static final String TEST_OWNER_USERNAME="User1";
+	private static final int		TEST_OWNER_ID			= 1;
 
-	@Autowired
-	private PickUpRequestController pickUpRequestController;
-
-	@MockBean
-	private PickUpRequestService pickUpRequestService;
-
-	@MockBean
-	private AuthoritiesService authoritiesService;
-
-	@MockBean
-	private OwnerService ownerService;
-
-	@MockBean
-	private PetService petService;
+	private static final String		TEST_OWNER_USERNAME		= "User1";
 
 	@Autowired
-	private MockMvc mockMvc;
+	private PickUpRequestController	pickUpRequestController;
+
+	@MockBean
+	private PickUpRequestService	pickUpRequestService;
+
+	@MockBean
+	private AuthoritiesService		authoritiesService;
+
+	@MockBean
+	private OwnerService			ownerService;
+
+	@MockBean
+	private PetService				petService;
+
+	@Autowired
+	private MockMvc					mockMvc;
+
 
 	@BeforeEach
 	void setup() {
@@ -68,8 +72,8 @@ public class PickUpRequestControllerTests {
 		pickUp.setIsAccepted(false);
 		Owner owner = new Owner();
 		User user = new User();
-		user.setUsername("User1");
-		user.setPassword("1234");
+		user.setUsername("owner1");
+		user.setPassword("123");
 		user.setEnabled(true);
 		owner.setFirstName("Paco");
 		owner.setLastName("Gonzalez");
@@ -96,6 +100,7 @@ public class PickUpRequestControllerTests {
 		BDDMockito.given(this.ownerService.findOwnerById(PickUpRequestControllerTests.TEST_OWNER_ID)).willReturn(owner);
 		BDDMockito.given(this.petService.findPetTypes()).willReturn(temp2);
 		BDDMockito.given(this.pickUpRequestService.findPickUpRequestsByOwnerUsername(PickUpRequestControllerTests.TEST_OWNER_USERNAME)).willReturn(pickUps);
+		BDDMockito.given(this.pickUpRequestService.findPickUpRequestByPickUpRequestId(PickUpRequestControllerTests.TEST_PICK_UP_REQUEST_ID)).willReturn(pickUp);
 		BDDMockito.given(this.pickUpRequestService.findOwnerByUsername(PickUpRequestControllerTests.TEST_OWNER_USERNAME)).willReturn(owner);
 
 	}
@@ -106,152 +111,151 @@ public class PickUpRequestControllerTests {
 
 	// Caso positivo: redirige a /vets/pick-up-requests
 
-	@WithMockUser(username = "owner1", roles = { "owner" }, password = "123")
+	@WithMockUser(username = "vet1", roles = {
+		"vet"
+	}, password = "123")
 	@Test
-	void testProcessDeleteBeautyCenterFormSuccess() throws Exception {
-		this.mockMvc
-				.perform(MockMvcRequestBuilders
-						.get("/vets/pick-up-requests/{pickUpId}/delete",
-								PickUpRequestControllerTests.TEST_PICK_UP_REQUEST_ID)
-						.with(SecurityMockMvcRequestPostProcessors.csrf()))
-				.andExpect(MockMvcResultMatchers.status().is3xxRedirection())
-				.andExpect(MockMvcResultMatchers.view().name("redirect:/vets/pick-up-requests"));
+	void testProcessDeletePickUpRequestAsVetSuccess() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/vets/pick-up-requests/{pickUpId}/delete", PickUpRequestControllerTests.TEST_PICK_UP_REQUEST_ID).with(SecurityMockMvcRequestPostProcessors.csrf()))
+			.andExpect(MockMvcResultMatchers.status().is3xxRedirection()).andExpect(MockMvcResultMatchers.view().name("redirect:/vets/pick-up-requests"));
 	}
 
 	// Historia de usuario 16
 	// Caso positivo acceder al formulario de crear
-	@WithMockUser(username = "owner1", roles = { "owner" }, password = "123")
+	@WithMockUser(username = "owner1", roles = {
+		"owner"
+	}, password = "123")
 	@Test
 	void testInitCreationForm() throws Exception {
-		this.mockMvc
-				.perform(MockMvcRequestBuilders.get("/owners/{ownerId}/pick-up-requests/new",
-						PickUpRequestControllerTests.TEST_OWNER_ID))
-				.andExpect(MockMvcResultMatchers.status().isOk())
-				.andExpect(MockMvcResultMatchers.view().name("pick-up-requests/createOrUpdatePickUpRequestForm"))
-				.andExpect(MockMvcResultMatchers.model().attributeExists("pickUpRequest"));
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/owners/{ownerId}/pick-up-requests/new", PickUpRequestControllerTests.TEST_OWNER_ID)).andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.view().name("pick-up-requests/createOrUpdatePickUpRequestForm")).andExpect(MockMvcResultMatchers.model().attributeExists("pickUpRequest"));
 	}
 
 	// Caso negativo acceder con id invalido
-	@WithMockUser(username = "owner1", roles = { "owner" }, password = "123")
+	@WithMockUser(username = "owner1", roles = {
+		"owner"
+	}, password = "123")
 	@Test
 	void testInitCreationFormError() throws Exception {
-		this.mockMvc.perform(MockMvcRequestBuilders.get("/owners/{ownerId}/pick-up-requests/new", 2))
-				.andExpect(MockMvcResultMatchers.status().isOk())
-				.andExpect(MockMvcResultMatchers.view().name("exception"));
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/owners/{ownerId}/pick-up-requests/new", 2)).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.view().name("exception"));
 	}
 
 	// Caso positivo creacion con exito
-	@WithMockUser(username = "owner1", roles = { "owner" }, password = "123")
+	@WithMockUser(username = "owner1", roles = {
+		"owner"
+	}, password = "123")
 	@Test
 	void testProcessCreationFormSuccess() throws Exception {
-		this.mockMvc
-				.perform(MockMvcRequestBuilders
-						.post("/owners/{ownerId}/pick-up-requests/new", PickUpRequestControllerTests.TEST_OWNER_ID)
-						.with(SecurityMockMvcRequestPostProcessors.csrf()).param("description", "descripcion")
-						.param("physicalStatus", "good").param("address", "Calle 1").param("petType", "dog")
-						.param("isAccepted", "true"))
-				.andExpect(MockMvcResultMatchers.status().is3xxRedirection())
-				.andExpect(MockMvcResultMatchers.view().name("redirect:/"));
+		this.mockMvc.perform(MockMvcRequestBuilders.post("/owners/{ownerId}/pick-up-requests/new", PickUpRequestControllerTests.TEST_OWNER_ID).with(SecurityMockMvcRequestPostProcessors.csrf()).param("description", "descripcion")
+			.param("physicalStatus", "good").param("address", "Calle 1").param("petType", "dog").param("isAccepted", "true")).andExpect(MockMvcResultMatchers.status().is3xxRedirection()).andExpect(MockMvcResultMatchers.view().name("redirect:/"));
 	}
 
 	// Caso positivo creacion sin descripcion
-	@WithMockUser(username = "owner1", roles = { "owner" }, password = "123")
+	@WithMockUser(username = "owner1", roles = {
+		"owner"
+	}, password = "123")
 	@Test
 	void testProcessCreationFormNoDescriptionSuccess() throws Exception {
-		this.mockMvc
-				.perform(MockMvcRequestBuilders
-						.post("/owners/{ownerId}/pick-up-requests/new", PickUpRequestControllerTests.TEST_OWNER_ID)
-						.with(SecurityMockMvcRequestPostProcessors.csrf()).param("physicalStatus", "good")
-						.param("address", "Calle 1").param("petType", "dog").param("isAccepted", "true"))
-				.andExpect(MockMvcResultMatchers.status().is3xxRedirection())
-				.andExpect(MockMvcResultMatchers.view().name("redirect:/"));
+		this.mockMvc.perform(MockMvcRequestBuilders.post("/owners/{ownerId}/pick-up-requests/new", PickUpRequestControllerTests.TEST_OWNER_ID).with(SecurityMockMvcRequestPostProcessors.csrf()).param("physicalStatus", "good").param("address", "Calle 1")
+			.param("petType", "dog").param("isAccepted", "true")).andExpect(MockMvcResultMatchers.status().is3xxRedirection()).andExpect(MockMvcResultMatchers.view().name("redirect:/"));
 	}
 
 	// Caso positivo creacion sin descripcion ni estado fisico
-	@WithMockUser(username = "owner1", roles = { "owner" }, password = "123")
+	@WithMockUser(username = "owner1", roles = {
+		"owner"
+	}, password = "123")
 	@Test
 	void testProcessCreationFormNoDescriptionAndPhysicalStatusSuccess() throws Exception {
-		this.mockMvc
-				.perform(MockMvcRequestBuilders
-						.post("/owners/{ownerId}/pick-up-requests/new", PickUpRequestControllerTests.TEST_OWNER_ID)
-						.with(SecurityMockMvcRequestPostProcessors.csrf()).param("address", "Calle 1")
-						.param("petType", "dog").param("isAccepted", "true"))
-				.andExpect(MockMvcResultMatchers.status().is3xxRedirection())
-				.andExpect(MockMvcResultMatchers.view().name("redirect:/"));
+		this.mockMvc.perform(
+			MockMvcRequestBuilders.post("/owners/{ownerId}/pick-up-requests/new", PickUpRequestControllerTests.TEST_OWNER_ID).with(SecurityMockMvcRequestPostProcessors.csrf()).param("address", "Calle 1").param("petType", "dog").param("isAccepted", "true"))
+			.andExpect(MockMvcResultMatchers.status().is3xxRedirection()).andExpect(MockMvcResultMatchers.view().name("redirect:/"));
 	}
 
 	// Caso negativo creacion sin direccion
-	@WithMockUser(username = "owner1", roles = { "owner" }, password = "123")
+	@WithMockUser(username = "owner1", roles = {
+		"owner"
+	}, password = "123")
 	@Test
 	void testProcessCreationFormNoAddressIncorrect() throws Exception {
 		this.mockMvc
-				.perform(MockMvcRequestBuilders
-						.post("/owners/{ownerId}/pick-up-requests/new", PickUpRequestControllerTests.TEST_OWNER_ID)
-						.with(SecurityMockMvcRequestPostProcessors.csrf()).param("description", "descripcion")
-						.param("physicalStatus", "good").param("petType", "dog").param("isAccepted", "true"))
-				.andExpect(MockMvcResultMatchers.model().attributeHasErrors("pickUpRequest"))
-				.andExpect(MockMvcResultMatchers.model().attributeHasFieldErrors("pickUpRequest", "address"))
-				.andExpect(MockMvcResultMatchers.status().isOk())
-				.andExpect(MockMvcResultMatchers.view().name("pick-up-requests/createOrUpdatePickUpRequestForm"));
+			.perform(MockMvcRequestBuilders.post("/owners/{ownerId}/pick-up-requests/new", PickUpRequestControllerTests.TEST_OWNER_ID).with(SecurityMockMvcRequestPostProcessors.csrf()).param("description", "descripcion").param("physicalStatus", "good")
+				.param("petType", "dog").param("isAccepted", "true"))
+			.andExpect(MockMvcResultMatchers.model().attributeHasErrors("pickUpRequest")).andExpect(MockMvcResultMatchers.model().attributeHasFieldErrors("pickUpRequest", "address")).andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.view().name("pick-up-requests/createOrUpdatePickUpRequestForm"));
 	}
 
 	// Caso negativo creacion sin petType
-	@WithMockUser(username = "owner1", roles = { "owner" }, password = "123")
+	@WithMockUser(username = "owner1", roles = {
+		"owner"
+	}, password = "123")
 	@Test
 	void testProcessCreationFormNoPetTypeIncorrect() throws Exception {
-		Mockito.when(this.pickUpRequestService.savePickUpRequest(ArgumentMatchers.any()))
-				.thenThrow(new NoPetTypeException());
+		Mockito.when(this.pickUpRequestService.savePickUpRequest(ArgumentMatchers.any())).thenThrow(new NoPetTypeException());
 		this.mockMvc
-				.perform(MockMvcRequestBuilders
-						.post("/owners/{ownerId}/pick-up-requests/new", PickUpRequestControllerTests.TEST_OWNER_ID)
-						.with(SecurityMockMvcRequestPostProcessors.csrf()).param("description", "descripcion")
-						.param("physicalStatus", "good").param("address", "Calle 1").param("petType", "")
-						.param("isAccepted", "true"))
-				.andExpect(MockMvcResultMatchers.model().attributeHasErrors("pickUpRequest"))
-				.andExpect(MockMvcResultMatchers.model().attributeHasFieldErrors("pickUpRequest", "petType"))
-				.andExpect(MockMvcResultMatchers.status().isOk())
-				.andExpect(MockMvcResultMatchers.view().name("pick-up-requests/createOrUpdatePickUpRequestForm"));
+			.perform(MockMvcRequestBuilders.post("/owners/{ownerId}/pick-up-requests/new", PickUpRequestControllerTests.TEST_OWNER_ID).with(SecurityMockMvcRequestPostProcessors.csrf()).param("description", "descripcion").param("physicalStatus", "good")
+				.param("address", "Calle 1").param("petType", "").param("isAccepted", "true"))
+			.andExpect(MockMvcResultMatchers.model().attributeHasErrors("pickUpRequest")).andExpect(MockMvcResultMatchers.model().attributeHasFieldErrors("pickUpRequest", "petType")).andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.view().name("pick-up-requests/createOrUpdatePickUpRequestForm"));
 	}
 
 	// Caso negativo creacion sin aceptado
-	@WithMockUser(username = "owner1", roles = { "owner" }, password = "123")
+	@WithMockUser(username = "owner1", roles = {
+		"owner"
+	}, password = "123")
 	@Test
 	void testProcessCreationFormNoAcceptedIncorrect() throws Exception {
 		this.mockMvc
-				.perform(MockMvcRequestBuilders
-						.post("/owners/{ownerId}/pick-up-requests/new", PickUpRequestControllerTests.TEST_OWNER_ID)
-						.with(SecurityMockMvcRequestPostProcessors.csrf()).param("description", "descripcion")
-						.param("physicalStatus", "good").param("address", "Calle 1").param("petType", "")
-						.param("isAccepted", ""))
-				.andExpect(MockMvcResultMatchers.model().attributeHasErrors("pickUpRequest"))
-				.andExpect(MockMvcResultMatchers.model().attributeHasFieldErrors("pickUpRequest", "isAccepted"))
-				.andExpect(MockMvcResultMatchers.status().isOk())
-				.andExpect(MockMvcResultMatchers.view().name("pick-up-requests/createOrUpdatePickUpRequestForm"));
+			.perform(MockMvcRequestBuilders.post("/owners/{ownerId}/pick-up-requests/new", PickUpRequestControllerTests.TEST_OWNER_ID).with(SecurityMockMvcRequestPostProcessors.csrf()).param("description", "descripcion").param("physicalStatus", "good")
+				.param("address", "Calle 1").param("petType", "").param("isAccepted", ""))
+			.andExpect(MockMvcResultMatchers.model().attributeHasErrors("pickUpRequest")).andExpect(MockMvcResultMatchers.model().attributeHasFieldErrors("pickUpRequest", "isAccepted")).andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.view().name("pick-up-requests/createOrUpdatePickUpRequestForm"));
 	}
 	//Listado de peticiones
 	//Caso positivo listado correcto
 	@WithMockUser(username = "owner1", roles = {
-			"owner"
-		}, password = "123")
-		@Test
-		void testInitShowForm() throws Exception {
-			this.mockMvc.perform(MockMvcRequestBuilders.get("/owners/{ownerUsername}/pick-up-requests", 
-				PickUpRequestControllerTests.TEST_OWNER_USERNAME))
-			.andExpect(MockMvcResultMatchers.status().isOk())
-			.andExpect(MockMvcResultMatchers.model().attributeExists("pickUpRequests"))
-			.andExpect(MockMvcResultMatchers.view().name("pick-up-requests/pickUpRequestsList"));
-		}
+		"owner"
+	}, password = "123")
+	@Test
+	void testInitShowForm() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/owners/{ownerUsername}/pick-up-requests", PickUpRequestControllerTests.TEST_OWNER_USERNAME)).andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.model().attributeExists("pickUpRequests")).andExpect(MockMvcResultMatchers.view().name("pick-up-requests/pickUpRequestsList"));
+	}
 	//Caso negativo acceso invalido
-		@WithMockUser(username = "owner1", roles = {
-				"owner"
-			}, password = "123")
-			@Test
-			void testInitShowInvalidAccessForm() throws Exception {
-			BDDMockito.given(this.authoritiesService.isAuthor(PickUpRequestControllerTests.TEST_OWNER_USERNAME)).willReturn(false);
-			this.mockMvc.perform(MockMvcRequestBuilders.get("/owners/{ownerUsername}/pick-up-requests", "MicSker"))
-			.andExpect(MockMvcResultMatchers.status().isOk())
-			.andExpect(MockMvcResultMatchers.view().name("exception"));
-			}
-		
-		
+	@WithMockUser(username = "owner1", roles = {
+		"owner"
+	}, password = "123")
+	@Test
+	void testInitShowInvalidAccessForm() throws Exception {
+		BDDMockito.given(this.authoritiesService.isAuthor(PickUpRequestControllerTests.TEST_OWNER_USERNAME)).willReturn(false);
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/owners/{ownerUsername}/pick-up-requests", "MicSker")).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.view().name("exception"));
+	}
+
+	/*
+	 * Test de la historia de usuario 17
+	 */
+
+	// Get mapping de /owners/{ownerUsername}/pick-up-requests/{pickUpId}/delete
+
+	//Caso positivo, intento borrar una petición de recogida siendo el autor de dicha petición
+
+	@WithMockUser(username = "owner1", roles = {
+		"owner"
+	}, password = "123")
+	@Test
+	void testProcessDeleteOwnerPickUpRequestSuccess() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/owners/{ownerUsername}/pick-up-requests/{pickUpId}/delete", "owner1", PickUpRequestControllerTests.TEST_PICK_UP_REQUEST_ID).with(SecurityMockMvcRequestPostProcessors.csrf()))
+			.andExpect(MockMvcResultMatchers.status().is3xxRedirection()).andExpect(MockMvcResultMatchers.view().name("redirect:/owners/{ownerUsername}/pick-up-requests"));
+	}
+
+	//Caso negativo, no es el autor de la petición
+
+	@WithMockUser(username = "owner2", roles = {
+		"owner"
+	}, password = "1234")
+	@Test
+	void testProcessDeleteOwnerPickUpRequestError() throws Exception {
+		Mockito.when(this.authoritiesService.isAuthor(ArgumentMatchers.any())).thenThrow(new InvalidActivityException());
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/owners/{ownerUsername}/pick-up-requests/{pickUpId}/delete", "owner1", PickUpRequestControllerTests.TEST_PICK_UP_REQUEST_ID).with(SecurityMockMvcRequestPostProcessors.csrf()))
+			.andExpect(MockMvcResultMatchers.status().is3xxRedirection()).andExpect(MockMvcResultMatchers.view().name("redirect:/oups"));
+	}
 }
