5fad1b715b8f50bc7c969a455fa9027f52ddee67
gonguelun <gonguelun@alum.us.es>
2020-05-02 03:59:19 +0200

    Task 059: falta caso negativo

---
+++ src/test/java/org/springframework/samples/petclinic/ui/ListOwnerBeautyDatesAuthorError.java
@@ -0,0 +1,105 @@
+
+package org.springframework.samples.petclinic.ui;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ListOwnerBeautyDatesAuthorError {
+
+	private WebDriver		driver;
+	private String			baseUrl;
+	private boolean			acceptNextAlert		= true;
+	private StringBuffer	verificationErrors	= new StringBuffer();
+
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		String pathToGeckoDriver = "C:\\Users\\Gonzalo\\Desktop\\UNIVERSIDAD\\TERCERO\\DP2";
+		System.setProperty("webdriver.gecko.driver", pathToGeckoDriver + "\\geckodriver.exe");
+		this.driver = new FirefoxDriver();
+		this.baseUrl = "https://www.google.com/";
+		this.driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void testListOwnerBeautyDatesAuthorError() throws Exception {
+		this.driver.get("http://localhost:8080/");
+		this.driver.findElement(By.linkText("OWNER REGISTER")).click();
+		this.driver.findElement(By.id("firstName")).click();
+		this.driver.findElement(By.id("firstName")).clear();
+		this.driver.findElement(By.id("firstName")).sendKeys("b");
+		this.driver.findElement(By.id("lastName")).clear();
+		this.driver.findElement(By.id("lastName")).sendKeys("b");
+		this.driver.findElement(By.id("address")).clear();
+		this.driver.findElement(By.id("address")).sendKeys("b");
+		this.driver.findElement(By.id("city")).clear();
+		this.driver.findElement(By.id("city")).sendKeys("b");
+		this.driver.findElement(By.id("telephone")).clear();
+		this.driver.findElement(By.id("telephone")).sendKeys("123456789");
+		this.driver.findElement(By.id("user.username")).clear();
+		this.driver.findElement(By.id("user.username")).sendKeys("b");
+		this.driver.findElement(By.id("user.password")).clear();
+		this.driver.findElement(By.id("user.password")).sendKeys("b");
+		this.driver.findElement(By.id("add-owner-form")).submit();
+		this.driver.findElement(By.linkText("LOGIN")).click();
+		this.driver.findElement(By.id("username")).clear();
+		this.driver.findElement(By.id("username")).sendKeys("b");
+		this.driver.findElement(By.id("password")).clear();
+		this.driver.findElement(By.id("password")).sendKeys("b");
+		this.driver.findElement(By.id("password")).sendKeys(Keys.ENTER);
+		this.driver.get("http://localhost:8080/owners/owner1/beauty-dates");
+	}
+
+	@AfterEach
+	public void tearDown() throws Exception {
+		this.driver.quit();
+		String verificationErrorString = this.verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			Assert.fail(verificationErrorString);
+		}
+	}
+
+	private boolean isElementPresent(final By by) {
+		try {
+			this.driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+
+	private boolean isAlertPresent() {
+		try {
+			this.driver.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
+
+	private String closeAlertAndGetItsText() {
+		try {
+			Alert alert = this.driver.switchTo().alert();
+			String alertText = alert.getText();
+			if (this.acceptNextAlert) {
+				alert.accept();
+			} else {
+				alert.dismiss();
+			}
+			return alertText;
+		} finally {
+			this.acceptNextAlert = true;
+		}
+	}
+}
