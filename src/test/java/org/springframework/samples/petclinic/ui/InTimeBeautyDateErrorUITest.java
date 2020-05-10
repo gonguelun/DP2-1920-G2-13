649d5bf64a5f4805f2c34116aa818a7e343ac352
gonguelun <gonguelun@alum.us.es>
2020-05-10 19:23:38 +0200

    finished

--- src/test/java/org/springframework/samples/petclinic/ui/InTimeBeautyDateErrorUITest.java
+++ src/test/java/org/springframework/samples/petclinic/ui/InTimeBeautyDateErrorUITest.java
@@ -35,6 +35,7 @@ public class InTimeBeautyDateErrorUITest {
 
 	@BeforeEach
 	public void setUp() throws Exception {
+		System.setProperty("webdriver.gecko.driver", System.getenv("webdriver.gecko.driver"));
 		this.driver = new FirefoxDriver();
 		this.baseUrl = "https://www.google.com/";
 		this.driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
