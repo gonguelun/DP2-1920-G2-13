4ef6aa767becbacc23a3a0cf220aa4c8c15360fd
Mario David <marjimhar@alum.us.es>
2020-06-02 19:16:00 +0200

    Final?

--- src/test/java/org/springframework/samples/petclinic/ui/UpdateBeauticianSuccessUITest.java
+++ src/test/java/org/springframework/samples/petclinic/ui/UpdateBeauticianSuccessUITest.java
@@ -1,19 +1,5 @@
+
 package org.springframework.samples.petclinic.ui;
-
-import java.util.regex.Pattern;
-import java.util.concurrent.TimeUnit;
-import org.junit.*;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
-
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.concurrent.TimeUnit;
 
@@ -52,67 +38,65 @@ public class UpdateBeauticianSuccessUITest {
 		this.driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 
-	  @Test
-	  public void testUpdateBeauticianSuccessUI() throws Exception {
-		 this.driver.get("http://localhost:" + this.port);
-	    driver.findElement(By.linkText("LOGIN")).click();
-	    driver.findElement(By.id("username")).clear();
-	    driver.findElement(By.id("username")).sendKeys("f");
-	    driver.findElement(By.id("password")).clear();
-	    driver.findElement(By.id("password")).sendKeys("f");
-	    driver.findElement(By.xpath("//button[@type='submit']")).click();
-	    driver.findElement(By.linkText("BEAUTICIAN")).click();
-	    assertEquals("f f", driver.findElement(By.xpath("//b")).getText());
-	    driver.findElement(By.xpath("//body/div/div")).click();
-	    driver.findElement(By.linkText("Edit Beautician")).click();
-	    driver.findElement(By.id("firstName")).click();
-	    driver.findElement(By.id("firstName")).clear();
-	    driver.findElement(By.id("firstName")).sendKeys("beauticianmodificado");
-	    driver.findElement(By.xpath("//button[@type='submit']")).click();
-	    assertEquals("beauticianmodificado f", driver.findElement(By.xpath("//b")).getText());
-	  }
-
-	  @After
-	  public void tearDown() throws Exception {
-	    driver.quit();
-	    String verificationErrorString = verificationErrors.toString();
-	    if (!"".equals(verificationErrorString)) {
-	      fail(verificationErrorString);
-	    }
-	  }
-
-	  private boolean isElementPresent(By by) {
-	    try {
-	      driver.findElement(by);
-	      return true;
-	    } catch (NoSuchElementException e) {
-	      return false;
-	    }
-	  }
-
-	  private boolean isAlertPresent() {
-	    try {
-	      driver.switchTo().alert();
-	      return true;
-	    } catch (NoAlertPresentException e) {
-	      return false;
-	    }
-	  }
-
-	  private String closeAlertAndGetItsText() {
-	    try {
-	      Alert alert = driver.switchTo().alert();
-	      String alertText = alert.getText();
-	      if (acceptNextAlert) {
-	        alert.accept();
-	      } else {
-	        alert.dismiss();
-	      }
-	      return alertText;
-	    } finally {
-	      acceptNextAlert = true;
-	    }
-	  }
+	@Test
+	public void testUpdateBeauticianSuccessUI() throws Exception {
+		this.driver.get("http://localhost:" + this.port);
+		this.driver.findElement(By.linkText("LOGIN")).click();
+		this.driver.findElement(By.id("username")).clear();
+		this.driver.findElement(By.id("username")).sendKeys("f");
+		this.driver.findElement(By.id("password")).clear();
+		this.driver.findElement(By.id("password")).sendKeys("f");
+		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
+		this.driver.findElement(By.linkText("BEAUTICIAN")).click();
+		Assert.assertEquals("f f", this.driver.findElement(By.xpath("//b")).getText());
+		this.driver.findElement(By.xpath("//body/div/div")).click();
+		this.driver.findElement(By.linkText("Edit Beautician")).click();
+		this.driver.findElement(By.id("firstName")).click();
+		this.driver.findElement(By.id("firstName")).clear();
+		this.driver.findElement(By.id("firstName")).sendKeys("beauticianmodificado");
+		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
+		Assert.assertEquals("beauticianmodificado f", this.driver.findElement(By.xpath("//b")).getText());
 	}
 
+	@AfterEach
+	public void tearDown() throws Exception {
+		this.driver.quit();
+		String verificationErrorString = this.verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			Assert.fail(verificationErrorString);
+		}
+	}
 
+	private boolean isElementPresent(final By by) {
+		try {
+			this.driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+
+	private boolean isAlertPresent() {
+		try {
+			this.driver.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
+
+	private String closeAlertAndGetItsText() {
+		try {
+			Alert alert = this.driver.switchTo().alert();
+			String alertText = alert.getText();
+			if (this.acceptNextAlert) {
+				alert.accept();
+			} else {
+				alert.dismiss();
+			}
+			return alertText;
+		} finally {
+			this.acceptNextAlert = true;
+		}
+	}
+}
